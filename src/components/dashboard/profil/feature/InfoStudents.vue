9cabca78887508fc226e9eabc90ad26b65eb59a4
ManouBlog <adjobikadjopierre27@gmail.com>
2026-03-26 11:51:50 +0100

    add statut pro, veteran, artisan

--- src/components/dashboard/profil/feature/InfoStudents.vue
+++ src/components/dashboard/profil/feature/InfoStudents.vue
@@ -201,8 +201,9 @@ export default {
       <div class="info-header" style="display: flex; justify-content: space-between">
         <h1 class="fw-bold my-3">Informations personnelles</h1>
         <div>
+    
           <button 
-          v-if="user.user.statut.statut != 'etudiant'"
+          v-if="user.user.statut.statut != 'etudiant' && !user.is_verified"
           style="
           width:auto;
           padding:0.5em;
