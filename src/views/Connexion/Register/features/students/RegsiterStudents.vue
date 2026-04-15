d573664f7641f8246edaf26c32c0a784ba6c2c34
ManouBlog <adjobikadjopierre27@gmail.com>
2026-04-15 17:55:56 +0100

    new

--- src/views/Connexion/Register/features/students/RegsiterStudents.vue
+++ src/views/Connexion/Register/features/students/RegsiterStudents.vue
@@ -257,7 +257,8 @@ idStatutChoice:Object
           this.formState.profilHybride = [];
           this.formState.ville = "";
           this.formState.commune = "";
-          this.formState.statut_talent = ""
+          this.formState.statut_talent = "";
+          this.formState.uploadCNI = null
         }
         console.log("formState.optionsAnswer", value);
         
