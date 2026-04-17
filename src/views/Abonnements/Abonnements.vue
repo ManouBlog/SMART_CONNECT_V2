969f95553313ed726985257705b0484156852e0c
ManouBlog <adjobikadjopierre27@gmail.com>
2026-04-17 17:31:18 +0100

    new

--- src/views/Abonnements/Abonnements.vue
+++ src/views/Abonnements/Abonnements.vue
@@ -105,28 +105,19 @@ onMounted(async () => {
   console.log("artisan ready", abonnementsArtisan.value)
 })
 
-// onMounted(async () => {
-//   text0.value = await translateStore.handleTranslate("Choisissez votre formule")
-//   reference.value = route.params.reference
-//   profileAbonnement.value = props.ProfilAbonnement ? props.ProfilAbonnement:store.state.user?.user?.statut?.statut
-//   activeTab.value = props.ProfilAbonnement ? props.ProfilAbonnement:'Etudiant'
-//   console.log("storeUSER",store.state.user?.user?.statut?.statut)
-//     if(reference.value){
-//     doVerificationAbonnement(reference.value)
-//     } 
-//   await handleAbonement();
-// });
 </script>
 
 <template>
   <div class="wrapped myconteneur">
     <h1 class="text-center main-color">{{text0}}</h1>
-    <n-card>
+     <div v-if="!props.ProfilAbonnement">
+ <n-card>
        <div class="d-flex justify-content-center">
      <p style="background:#df3535;color:white;">
       Tout abonnement existant sera automatiquement remplacé par votre nouveau choix
      </p>
       </div>
+      
       <n-tabs type="line" size="large" animated justify-content="center"
       v-model:value="activeTab"
       >
@@ -261,6 +252,156 @@ onMounted(async () => {
 
 </n-tabs>
     </n-card>
+     </div>
+      <div v-else>
+    <n-card>
+       <div class="d-flex justify-content-center">
+     <p style="background:#df3535;color:white;">
+      Tout abonnement existant sera automatiquement remplacé par votre nouveau choix
+     </p>
+      </div>
+      
+      <n-tabs type="line" size="large" animated justify-content="center"
+      v-model:value="activeTab"
+      >
+
+  <!-- Etudiant -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s => ['Etudiant'].includes(s.statut)) 
+        && profileAbonnement === 'Etudiant'
+      )
+    "
+    :name="defaulValueTranslate === 'fr' ? 'Etudiant' : 'Student'"
+    :tab="defaulValueTranslate === 'fr' ? 'Etudiant' : 'Student'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Etudiant"
+    />
+  </n-tab-pane>
+
+  <!-- Entreprise -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s => ['Entreprise'].includes(s.statut)) 
+        && profileAbonnement === 'Entreprise'
+      )
+    "
+    :name="'Entreprise'"
+    :tab="'Entreprise'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Entreprise"
+      :tabsSubAbonnement="abonnements
+        .filter(item => item.categorie.categorie.toLowerCase().includes('entreprise'))
+        .map(item => ({
+          label: item.categorie.categorie,
+          id: item.categorie.categorie
+        }))
+      "
+      :subAbonnement="abonnements
+        .filter(item => item.categorie.categorie.toLowerCase().includes('entreprise'))
+      "
+    />
+  </n-tab-pane>
+
+  <!-- Particulier -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s => ['Particulier'].includes(s.statut)) 
+        && profileAbonnement === 'Particulier'
+      )
+    "
+    :name="defaulValueTranslate === 'fr' ? 'Particulier' : 'Company'"
+    :tab="defaulValueTranslate === 'fr' ? 'Particulier' : 'Company'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Particulier"
+    />
+  </n-tab-pane>
+
+  <!-- Artisan -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s => ['Artisan'].includes(s.statut)) 
+        && profileAbonnement === 'Artisan'
+      )
+    "
+    :name="defaulValueTranslate === 'fr' ? 'Artisan' : 'Company'"
+    :tab="defaulValueTranslate === 'fr' ? 'Artisan' : 'Company'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Artisan"
+      :tabsSubAbonnement="abonnementsArtisan"
+      :subAbonnement="abonnements
+        .filter(item => item.categorie.categorie.toLowerCase().includes('artisan'))
+      "
+    />
+  </n-tab-pane>
+
+  <!-- Professionnel -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s => ['Professionnel'].includes(s.statut)) 
+        && profileAbonnement === 'Professionnel'
+      )
+    "
+    :name="defaulValueTranslate === 'fr' ? 'Professionnel' : 'Company'"
+    :tab="defaulValueTranslate === 'fr' ? 'Professionnel' : 'Company'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Professionnel"
+    />
+  </n-tab-pane>
+
+  <!-- Vétéran -->
+  <n-tab-pane
+    v-if="
+      !store.state.user ||
+      (
+        store.state.user?.user?.statuses.some(s =>
+          ['Vétéran', 'veteran'].includes(s.statut)
+        ) && profileAbonnement === 'veteran'
+      )
+    "
+    :name="'Vétéran'"
+    :tab="'Vétéran'"
+  >
+    <ContainerAbonnements
+      :abonnements="abonnements"
+      type_abonnements="Vétéran"
+      :tabsSubAbonnement="abonnements
+        .filter(item => item.categorie.categorie.toLowerCase().includes('vétéran'))
+        .map(item => ({
+          label: item.categorie.categorie,
+          id: item.categorie.categorie
+        }))
+      "
+      :subAbonnement="abonnements
+        .filter(item => item.categorie.categorie.toLowerCase().includes('vétéran'))
+      "
+    />
+  </n-tab-pane>
+
+</n-tabs>
+    </n-card>
+      </div>
+   
   </div>
 </template>
 <style scoped>
