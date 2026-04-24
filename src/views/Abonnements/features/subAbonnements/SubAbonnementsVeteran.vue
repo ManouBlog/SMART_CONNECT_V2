847d5899724357ccb18840df08955f230b4aa059
ManouBlog <adjobikadjopierre27@gmail.com>
2026-04-24 23:39:19 +0100

    new

--- src/views/Abonnements/features/subAbonnements/SubAbonnementsVeteran.vue
+++ src/views/Abonnements/features/subAbonnements/SubAbonnementsVeteran.vue
@@ -96,7 +96,6 @@ profilHybrideRecuperer.value = storeAbonnement?.profilHybride?.length
           ? 'abonnement-classique_etudiant'
           : 'abonnement-classique_entreprise'
       "
-      style="flex: 1;"
     >
    
       <h1 class="text-center main-color">
