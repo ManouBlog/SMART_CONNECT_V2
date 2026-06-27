75029e1587496edf53a6dbbd13f228c4ffed0107
ManouBlog <adjobikadjopierre27@gmail.com>
2026-06-27 15:49:30 +0200

    new

--- src/views/Abonnements/features/subAbonnements/SubAbonnementsEntreprise.vue
+++ src/views/Abonnements/features/subAbonnements/SubAbonnementsEntreprise.vue
@@ -58,61 +58,68 @@ const handleCreateEntrepriseByYear=(payload)=>{
 
         console.log("DATAEND",data)
         
-// const formData = new FormData();
+const formData = new FormData();
 
-// // On vérifie chaque valeur avant l'ajout
-// if (data.abonement_id) formData.append('abonement_id', data.abonement_id);
-// if (data.channels && data.channels !== 'undefined') formData.append('channels', data.channels);
-// if (data.mode_payment) formData.append('mode_payment', data.mode_payment);
+// On vérifie chaque valeur avant l'ajout
+if (data.abonement_id) formData.append('abonement_id', data.abonement_id);
+if (data.channels && data.channels !== 'undefined') formData.append('channels', data.channels);
+if (data.mode_payment) formData.append('mode_payment', data.mode_payment);
 
 
-// if (data.transaction_id) formData.append('transaction_id', data.transaction_id);
-// if (data.isAddProfilHybride !== undefined) formData.append('isAddProfilHybride', data.isAddProfilHybride);
-// if (data.statut_base) formData.append('statut_base', data.statut_base);
-// if (data.isChangeProfil !== undefined) formData.append('isChangeProfil', data.isChangeProfil);
+if (data.transaction_id) formData.append('transaction_id', data.transaction_id);
+if (data.isAddProfilHybride !== undefined) formData.append('isAddProfilHybride', data.isAddProfilHybride);
+if (data.statut_base) formData.append('statut_base', data.statut_base);
+if (data.isChangeProfil !== undefined) formData.append('isChangeProfil', data.isChangeProfil);
 
-// if (data.profilHybride?.length) {
-//   data.profilHybride
-//     .map(item => item.id)
-//     .forEach(id => {
-//       formData.append("profilHybride[]", id);
-//     });
-// }
-// if (data.cni) formData.append('cni', data.cni_gerant);
-// if (data.registre_pdf) formData.append('registre_pdf', data.registre_pdf);
-// if (data.logo) formData.append('logo', data.logo);
+if (data.profilHybride?.length) {
+  data.profilHybride
+    .map(item => item.id)
+    .forEach(id => {
+      formData.append("profilHybride[]", id);
+    });
+}
+if (data.cni) formData.append('cni', data.cni_gerant);
+if (data.registre_pdf) formData.append('registre_pdf', data.registre_pdf);
+if (data.logo) formData.append('logo', data.logo);
 
-// if (data.addProfilHybride?.length) {
-//   data.addProfilHybride
-//     .map(item => item.id)
-//     .forEach(id => {
-//       formData.append("addProfilHybrideOnly[]", id);
-//     });
-// }
+if (data.addProfilHybride?.length) {
+  data.addProfilHybride
+    .map(item => item.id)
+    .forEach(id => {
+      formData.append("addProfilHybrideOnly[]", id);
+    });
+}
 
-// // Autres champs
-// if (data.nom) formData.append('nom', data.nom);
-// if (data.expertise) formData.append('expertise', data.expertise);
-// if (data.photo) formData.append('photo', data.photo);
-// if (data.diplome) formData.append('diplome', data.diplome);
-// if (data.ville) formData.append('ville', data.ville);
-// if (data.commune) formData.append('commune', data.commune);
-// if (data.quartier) formData.append('quartier', data.quartier);
-// if (data.statut_professionnel_artisan) formData.append('statut_professionnel_artisan', data.statut_professionnel_artisan);
-// if (data.statut_entreprise) formData.append('statut_entreprise', data.statut_entreprise);
+if (data.email_cc?.length) {
+  data?.email_cc?.forEach(item => {
+      formData.append("email_cc[]", item);
+    });
+}
 // if (data.email_cc) formData.append('email_cc', data.email_cc);
-// if (data.Phonegerant) formData.append('Phonegerant', data.Phonegerant);
-// if (data.gerant) formData.append('gerant', data.gerant);
-// if (data.email) formData.append('email', data.email);
-// if (data.phone) formData.append('phone', data.phone);
-// if (data.ncc) formData.append('ncc', data.ncc);
-// if (data.juridique) formData.append('juridique', data.juridique);
-// if (data.matricule_cc) formData.append('matricule_cc', data.matricule_cc);
-// if (data.contact) formData.append('contact', data.contact);
 
-// console.log("formData21",formData)
+// Autres champs
+if (data.nom) formData.append('nom', data.nom);
+if (data.expertise) formData.append('expertise', data.expertise);
+if (data.photo) formData.append('photo', data.photo);
+if (data.diplome) formData.append('diplome', data.diplome);
+if (data.ville) formData.append('ville', data.ville);
+if (data.commune) formData.append('commune', data.commune);
+if (data.quartier) formData.append('quartier', data.quartier);
+if (data.statut_professionnel_artisan) formData.append('statut_professionnel_artisan', data.statut_professionnel_artisan);
+if (data.statut_entreprise) formData.append('statut_entreprise', data.statut_entreprise);
+
+if (data.Phonegerant) formData.append('Phonegerant', data.Phonegerant);
+if (data.gerant) formData.append('gerant', data.gerant);
+if (data.email) formData.append('email', data.email);
+if (data.phone) formData.append('phone', data.phone);
+if (data.ncc) formData.append('ncc', data.ncc);
+if (data.juridique) formData.append('juridique', data.juridique);
+if (data.matricule_cc) formData.append('matricule_cc', data.matricule_cc);
+if (data.contact) formData.append('contact', data.contact);
+
+console.log("formData21",formData)
      
-//   storeAbonnement.createAbonement(formData)
+  storeAbonnement.createAbonement(formData)
 }
 
 
