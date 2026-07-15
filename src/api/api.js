34e0b9b100a5091d63696852572984aed6420d62
ManouBlog <adjobikadjopierre27@gmail.com>
2026-07-15 11:07:15 +0100

    new

--- src/api/api.js
+++ src/api/api.js
@@ -35,34 +35,6 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
-// ⇒ Ajout : intercepteur de réponse pour le 401 Unauthenticated
-// instance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (
-//       error.response?.status === 401 &&
-//       error.response?.data?.message === "Unauthenticated."
-//     ) {
-//       localStorage.removeItem("token");
-//       // optionnel : d'autres clés locales
-//       localStorage.removeItem("user");
-//     }
-//      /*
-//     |--------------------------------------------------------------------------
-//     | 429 - Too Many Attempts
-//     |--------------------------------------------------------------------------
-//     */
-//     if (
-//       error.response?.status === 429 &&
-//       error.response?.data?.message === "Too Many Attempts."
-//     ) {
-//       alert(
-//         "Trop de tentatives détectées. Veuillez patienter quelques instants avant de réessayer."
-//       );
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -95,59 +67,12 @@ instance.interceptors.response.use(
 
       /*
       |--------------------------------------------------------------------------
-      | 403 - Forbidden
-      |--------------------------------------------------------------------------
-      */
-      case 403:
-        alert("Vous n'avez pas les autorisations nécessaires pour effectuer cette action.");
-        break;
-
-      /*
-      |--------------------------------------------------------------------------
-      | 404 - Not Found
-      |--------------------------------------------------------------------------
-      */
-      case 404:
-        alert("La ressource demandée est introuvable.");
-        break;
-
-      /*
-      |--------------------------------------------------------------------------
-      | 405 - Method Not Allowed
-      |--------------------------------------------------------------------------
-      */
-      case 405:
-        alert("Méthode non autorisée.");
-        break;
-
-      /*
-      |--------------------------------------------------------------------------
-      | 409 - Conflict
-      |--------------------------------------------------------------------------
-      */
-      case 409:
-        alert("Cette opération est en conflit avec les données existantes.");
-        break;
-
-      /*
-      |--------------------------------------------------------------------------
-      | 422 - Validation Error
-      |--------------------------------------------------------------------------
-      */
-      case 422:
-        alert("Certaines informations sont invalides. Veuillez les corriger.");
-        break;
-
-      /*
-      |--------------------------------------------------------------------------
       | 429 - Too Many Requests
       |--------------------------------------------------------------------------
       */
       case 429:
         alert("Trop de tentatives détectées. Veuillez patienter quelques instants avant de réessayer.");
         break;
-
-   
 
       /*
       |--------------------------------------------------------------------------
